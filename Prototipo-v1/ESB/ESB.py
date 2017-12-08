c7ffbd0c25de3bce081fcdb084142d00ebe73744
Alessandro Mattei <alessandro.mattei1@student.univaq.it>
2017-12-08 17:22:34 +0100

    bozza della funzione tipico_randomico

--- Prototipo-v1/ESB/ESB.py
+++ Prototipo-v1/ESB/ESB.py
@@ -22,16 +22,68 @@ def tipico():
 
 
 # Creazione oggetti in modo custum randomico
-def custom_randomico():
+def tipico_randomico():
+    total_robot = 0
     starting_number_cluster = 0
     starting_number_robot = 0
-    aree = random.randint(1, 3) #######################################
-    for a in range(aree):
-        number_of_cluster = random.randint(1, 100) #######################################
+    aree = random.randint(1, 2)
+    if aree == 1:
+        a = 0
+        number_of_cluster = 100
         Area.append(System.Area(a))
         starting_number_cluster = Area[a].set_cluster(number_of_cluster, starting_number_cluster)
         for c in range(len(Area[a].Cluster)):
-            number_of_robot = random.randint(1, 900) #########################################
+            number_of_robot = 900
+            starting_number_robot = Area[a].Cluster[c].set_robot(number_of_robot, starting_number_robot)
+
+        print(
+            'Ultimo robot di questa area (' + str(a) + ') ultimo cluster (' + str(starting_number_cluster) + ') : ',
+            Area[a].Cluster[c].Robot[Area[a].Cluster[c].Number_of_robot - 1].IDRobot)
+    elif aree == 2:
+        for a in range(aree):
+            if a == 0:
+                number_of_cluster = random.randint(40, 100)
+                Area.append(System.Area(a))
+                starting_number_cluster = Area[a].set_cluster(number_of_cluster, starting_number_cluster)
+                for c in range(len(Area[a].Cluster)):
+                    number_of_robot = random.randint(600, 900)
+                    starting_number_robot = Area[a].Cluster[c].set_robot(number_of_robot, starting_number_robot)
+                    total_robot = total_robot + number_of_robot
+                print(
+                    'Ultimo robot di questa area (' + str(a) + ') ultimo cluster (' + str(
+                        starting_number_cluster) + ') : ',
+                    Area[a].Cluster[c].Robot[Area[a].Cluster[c].Number_of_robot - 1].IDRobot)
+            else:
+                number_of_cluster = random.randint(30, 50)
+                Area.append(System.Area(a))
+                starting_number_cluster = Area[a].set_cluster(number_of_cluster, starting_number_cluster)
+                for c in range(len(Area[a].Cluster)):
+                    if c < (len(Area[a].Cluster)/2):
+                        number_of_robot = random.randint(400, 900)
+                        starting_number_robot = Area[a].Cluster[c].set_robot(number_of_robot, starting_number_robot)
+                        total_robot = total_robot + number_of_robot
+                    elif (c+1) == len(Area[a].Cluster):
+                        number_of_robot = 90000 - total_robot
+                        starting_number_robot = Area[a].Cluster[c].set_robot(number_of_robot, starting_number_robot)
+                print(
+                    'Ultimo robot di questa area (' + str(a) + ') ultimo cluster (' + str(
+                        starting_number_cluster) + ') : ',
+                    Area[a].Cluster[c].Robot[Area[a].Cluster[c].Number_of_robot - 1].IDRobot)
+                print(len(Area[a].Cluster[c].Robot))
+
+
+
+# Creazione oggetti in modo custum randomico
+def custom_randomico():
+    starting_number_cluster = 0
+    starting_number_robot = 0
+    aree = random.randint(1, 3)  #######################################
+    for a in range(aree):
+        number_of_cluster = random.randint(1, 100)  #######################################
+        Area.append(System.Area(a))
+        starting_number_cluster = Area[a].set_cluster(number_of_cluster, starting_number_cluster)
+        for c in range(len(Area[a].Cluster)):
+            number_of_robot = random.randint(1, 900)  #########################################
             starting_number_robot = Area[a].Cluster[c].set_robot(number_of_robot, starting_number_robot)
         print('Ultimo robot di questa area (' + str(a) + ') ultimo cluster (' + str(starting_number_cluster) + ') : ',
               Area[a].Cluster[c].Robot[Area[a].Cluster[c].Number_of_robot - 1].IDRobot)
@@ -57,3 +109,5 @@ def custum():
 # tipico()
 
 # custom_randomico()
+
+tipico_randomico()
