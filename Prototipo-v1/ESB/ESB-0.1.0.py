df591e17b773aae62a8eaf373dc0b4cc83d876ad
Alessandro Mattei <alessandro.mattei1@student.univaq.it>
2017-12-15 14:33:43 +0100

    Versione finale del ESB

--- Prototipo-v1/ESB/ESB-0.1.0.py
+++ Prototipo-v1/ESB/ESB-0.1.0.py
@@ -1,10 +1,9 @@
 import profile
-import requests
 import random
+import http.client
+import urllib.parse
+import System
 import threading
-import time
-import socket
-from System import *
 
 Aree = []
 total_robot = 0
@@ -12,7 +11,21 @@ numb_aree = 0
 starting_number_robot = 0
 starting_number_cluster = 0
 num_cluster = []
+headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
 
+
+class myThread(threading.Thread):
+    def __init__(self, threadID, name, counter, Aree):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+        self.area = Aree
+
+    def run(self):
+        #print("Starting " + self.name)
+        invio_thread(self.name, self.counter, self.area)
+        #print("Exiting " + self.name)
 
 ########################### CREAZIONE OGGETTI #################################
 def tipico_randomico():
@@ -20,7 +33,7 @@ def tipico_randomico():
     while total_robot < 90000:
         if total_robot < 50000:
             numb_of_cluster = random.randint(20, 50)
-            Aree.append(Area(numb_aree))
+            Aree.append(System.Area(numb_aree))
             starting_number_cluster = Aree[numb_aree].set_cluster(numb_of_cluster, starting_number_cluster)
             for c in range(numb_of_cluster):
                 numb_of_robot = random.randint(500, 1000)
@@ -34,7 +47,7 @@ def tipico_randomico():
             max_numb_of_cluster_remainder = int(numb_remainder / 600)
             min_numb_of_cluster_remainder = int(numb_remainder / 900)
             numb_of_cluster = random.randint(min_numb_of_cluster_remainder, max_numb_of_cluster_remainder)
-            Aree.append(Area(numb_aree))
+            Aree.append(System.Area(numb_aree))
             num_cluster.append(numb_of_cluster - 1)
             starting_number_cluster = Aree[numb_aree].set_cluster(numb_of_cluster, starting_number_cluster)
             numb_of_cluster_remainder = numb_of_cluster
@@ -72,52 +85,75 @@ def messaggio():
         robot = random.randint(0, numb_robot - 1)
     sensor = random.randint(1, 7)
     Aree[area].Cluster[cluster].Robot[robot].change_sensor_status("S" + str(sensor))
-    messaggio_json = {"ID_Area": Aree[area].IDArea, "ID_Cluster": Aree[area].Cluster[cluster].IDCluster,
-                      "Name_Of_Robot": Aree[area].Cluster[cluster].Robot[robot].IDRobot,
-                      "S1": str(Aree[area].Cluster[cluster].Robot[robot].Sensors['S1']),
-                      "S2": str(Aree[area].Cluster[cluster].Robot[robot].Sensors['S2']),
-                      "S3": str(Aree[area].Cluster[cluster].Robot[robot].Sensors['S3']),
-                      "S4": str(Aree[area].Cluster[cluster].Robot[robot].Sensors['S4']),
-                      "S5": str(Aree[area].Cluster[cluster].Robot[robot].Sensors['S5']),
-                      "S6": str(Aree[area].Cluster[cluster].Robot[robot].Sensors['S6']),
-                      "S7": str(Aree[area].Cluster[cluster].Robot[robot].Sensors['S7'])}
-    print(messaggio_json)
+    messaggio_json = urllib.parse.urlencode(
+        {"ID_Area": Aree[area].IDArea, "ID_Cluster": Aree[area].Cluster[cluster].IDCluster,
+         "Name_Of_Robot": Aree[area].Cluster[cluster].Robot[robot].IDRobot,
+         "S1": str(Aree[area].Cluster[cluster].Robot[robot].Sensors['S1']),
+         "S2": str(Aree[area].Cluster[cluster].Robot[robot].Sensors['S2']),
+         "S3": str(Aree[area].Cluster[cluster].Robot[robot].Sensors['S3']),
+         "S4": str(Aree[area].Cluster[cluster].Robot[robot].Sensors['S4']),
+         "S5": str(Aree[area].Cluster[cluster].Robot[robot].Sensors['S5']),
+         "S6": str(Aree[area].Cluster[cluster].Robot[robot].Sensors['S6']),
+         "S7": str(Aree[area].Cluster[cluster].Robot[robot].Sensors['S7'])})
     return messaggio_json
 
+def messaggio_thread(Areen):
+    global numb_aree, num_cluster
+    area = random.randint(0, numb_aree)
+    cluster = random.randint(0, num_cluster[area])
+    numb_robot = len(Areen[area].Cluster[cluster].Robot)
+    if (numb_robot - 1) == 0 or (numb_robot - 1) < 0:
+        robot = 0
+    else:
+        robot = random.randint(0, numb_robot - 1)
+    sensor = random.randint(1, 7)
+    Areen[area].Cluster[cluster].Robot[robot].change_sensor_status("S" + str(sensor))
+    messaggio_json = urllib.parse.urlencode(
+        {"ID_Area": Areen[area].IDArea, "ID_Cluster": Areen[area].Cluster[cluster].IDCluster,
+         "Name_Of_Robot": Areen[area].Cluster[cluster].Robot[robot].IDRobot,
+         "S1": str(Areen[area].Cluster[cluster].Robot[robot].Sensors['S1']),
+         "S2": str(Areen[area].Cluster[cluster].Robot[robot].Sensors['S2']),
+         "S3": str(Areen[area].Cluster[cluster].Robot[robot].Sensors['S3']),
+         "S4": str(Areen[area].Cluster[cluster].Robot[robot].Sensors['S4']),
+         "S5": str(Areen[area].Cluster[cluster].Robot[robot].Sensors['S5']),
+         "S6": str(Areen[area].Cluster[cluster].Robot[robot].Sensors['S6']),
+         "S7": str(Areen[area].Cluster[cluster].Robot[robot].Sensors['S7'])})
+    return messaggio_json
 
+####################### INVIO DATI ######################################
 
-####################### INVIO DATI ######################################à
-def invio(messaggio):
-    r = requests.post("http://192.168.64.2/httpHandler.php", data=messaggio)
-    #print(r.status_code, r.reason)
-    #print(r.text)
+def invio(msg):
+    conn = http.client.HTTPConnection("localhost")
+    conn.request("POST", "/httpHandler.php", msg, headers)
+    conn.close()
 
+exitFlag = 0
 
+def invio_thread(threadName, counter, areen):
+    while counter:
+        if exitFlag:
+            threadName.exit()
+        invio(messaggio_thread(areen))
+        counter = counter - 1
 #################### MAIN PROGRAM #############################
 def main():
     tipico_randomico()
-    invio()
-
-messaggio_j = {"ID_Area": "A1",
-               "ID_Cluster": "C67",
-               "Name_Of_Robot": "R49270",
-               "S1": "1",
-               "S2": "0",
-               "S3": "1",
-               "S4": "1",
-               "S5": "1",
-               "S6": "1",
-               "S7": "1"
-               }
-
-def more():
-    c = 200
+    c = 90000
     while c > 0:
+        invio(messaggio())
         c -= 1
-        invio(messaggio_j)
-#tipico_randomico()
 
+def main_thread():
+    print('start program...')
+    tipico_randomico()
+    Aree1 = Aree
+    Aree2 = Aree
+    thread1 = myThread(1, "Thread-1", 45000, Aree1)
+    thread2 = myThread(2, "Thread-2", 45000, Aree2)
+    thread1.start()
+    thread2.start()
+    thread1.join()
+    thread2.join()
+    print("Exiting Main Thread")
 
-
-
-profile.run('more()')
+profile.run('main_thread()')
